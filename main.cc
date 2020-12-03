b01ce1e762452afe9503d63a809b529196da76df
Yaroslav Afonin <Yaroslav.Afonin@nbkcpartners.com>
2020-12-03 19:24:53 +0000

    X

---
+++ main.cc
@@ -0,0 +1,50 @@
+/******************************************************************
+ * The Main program with the two functions. A simple
+ * example of creating and using a thread is provided.
+ ******************************************************************/
+
+#include "helper.h"
+
+void *producer (void *id);
+void *consumer (void *id);
+
+int main (int argc, char **argv)
+{
+
+  // TODO
+
+  pthread_t producerid;
+  int parameter = 5;
+
+  pthread_create (&producerid, NULL, producer, (void *) &parameter);
+
+  pthread_join (producerid, NULL);
+
+  cout << "Doing some work after the join" << endl;
+
+  return 0;
+}
+
+void *producer (void *parameter) 
+{
+
+  // TODO
+
+  int *param = (int *) parameter;
+
+  cout << "Parameter = " << *param << endl;
+
+  sleep (5);
+
+  cout << "\nThat was a good sleep - thank you \n" << endl;
+
+  pthread_exit(0);
+}
+
+void *consumer (void *id) 
+{
+    // TODO 
+
+  pthread_exit (0);
+
+}
