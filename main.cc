4f3a56542e7a7a035cc70e471e6db871b1116bbb
Yaroslav Afonin <Yaroslav.Afonin@nbkcpartners.com>
2020-12-05 10:48:19 +0000

    X

--- main.cc
+++ main.cc
@@ -127,19 +127,18 @@ for(int i = 0; i < number_of_consumers; i++ ) {
 }
 
 
-void *producer (void *parameter) 
+void *producer (void *id) 
 {
 /*
-(b) Add the required number of jobs to the circular queue, with each job being added once every
-1 – 5 seconds. 
+(b) Add the required number of jobs to the circular queue, with each job being added once every 1 – 5 seconds. 
 If a job is taken (and deleted) by the consumer, then another job can be produced which has the same id. 
 If the circular queue is full, block while waiting for an empty slot and if a slot doesn’t become
 available after 20 seconds, quit, even though you have not produced all the jobs.
 (d) Quit when there are no more jobs left to produce.
 */
-  cout << "\nEntered producer with id = " << *((int*)(parameter));
-  // Fill the buffer initially
 
+//  cout << "\nEntered producer with id = " << *producer_id;
+  int *producer_id = (int *)id;
   bool wait_within_time_limit = true;
   
   while (wait_within_time_limit){
@@ -148,29 +147,32 @@ available after 20 seconds, quit, even though you have not produced all the jobs
     int sleep_time = (rand() % 5) + 1;
     int duration = (rand() % 10) + 1; // Duration for each job should be between 1 – 10 seconds. 
 
-    int job_id = p;     // how to set id?
-    job J = job(job_id,duration);
-    cout << "Created job with id = " << job_id << " and duration = " << duration << " seconds.." << endl;
-
     sleep(sleep_time);
 
-// perform down op on semaphore + save timeout status
-// timeout = sem_timed_wait (sem_id, space, 20);
-// if (sem_timedwait( &sem, &tm) != -1 ) {break;}
-// clock_gettime(CLOCK_REALTIME, &tm);
-    //
+    auto it = find_if(Q.begin(), Q.end(), [&p](const job& obj) {return obj.id == p;})
+    // if it != Q.end();
+
+    int job_id = p;   
+    job J = job(job_id,duration);
+    //cout << "Created job with id = " << job_id << " and duration = " << duration << endl;
+
+    if (Q.size() < queue_size){} // if queue has space - then add item
 
     sem_wait(&empty_count);
     sem_wait(&queue_access_mutex);
     
-    Q.push(J);
+    Q.push(J);     // every 5 seconds = already slept
     
     sem_post(&queue_access_mutex);
     sem_post(&full_count);
 
-// (c) Print the status (example format given in example output.txt).
+    // else - wait 20 seconds 
+    // if (sem_timedwait( &sem, &tm) != -1 ) {break;} wait_within_time_limit = false; // 20 seconds
+    // clock_gettime(CLOCK_REALTIME, &tm); 
+
 std::ofstream ofs("output2.txt", std::ofstream::out);
-ofs << "writing job id = " << job_id;
+  cout << "Producer("<< *producer_id << "): Job id " << id << " sleeping for " << sleep_time << " seconds";
+  cout << "Producer("<< *producer_id << "): Job id " << id << " duration " << duration << " seconds";
 ofs.close();
       } // for loop ends
   } // while ends
@@ -181,38 +183,40 @@ ofs.close();
 void *consumer (void *id) 
 {
 /*
-(b) Take a job from the circular queue 
+(b) Take a job from the circular queue - critical
 ‘sleep’ for the duration specified. 
 If the circular queue is empty, 
   block while waiting for jobs and 
   quit if no jobs arrive within 20 seconds.
-(c) Print the status (example format given in example output.txt).
 (d) If there are no jobs left to consume, wait for 20 seconds to check if any new jobs are added,
 and if not, quit.
 */  
+  int *consumer_id = (int *) id;
 
   bool consumer_wait_within_time_limit = true;
 
   while(consumer_wait_within_time_limit) {
-    cout << "\nEntered consumer with id = " << *((int*)(id));
+//    cout << "\nEntered consumer with id = " << *consumer_id;
 
-    // change the time limit status??
-    //sem_timedwait();
-    // break out here
-   
+    // if Q.size() > 0 - enter critical section + take it    
     sem_wait(&full_count);
     sem_wait(&queue_access_mutex);
 
     job J = Q.front();
-    cout << "Consumer with id = " << *((int*)(id)) << " consuming job from front..";
+    //cout << "Consumer with id = " << *((int*)(id)) << " consuming job from front..";
     Q.pop();
 
     sem_post(&queue_access_mutex);
     sem_post(&empty_count);
+    // else - timedwait 20 seconds
+    // if not timedwait - break - consumer_wait_within_time_limit = false
 
-    sleep(J.duration);
+    sleep(J.duration);     // Consume
 
-
+std::ofstream ofs("output2.txt", std::ofstream::out);
+  cout << "Consumer("<< *consumer_id << "): Job id " << J.id << " executing sleep duration " << J.duration << endl;
+  cout << "Consumer("<< *consumer_id << "): Job id " << J.id << " completed" << endl;
+ofs.close();
   }
 
   pthread_exit (0);
