26f6351a5cbd82fa8fa52978346b95e46dc1ce89
Yaroslav Afonin <Yaroslav.Afonin@nbkcpartners.com>
2020-12-06 12:43:08 +0000

    X

--- main.cc
+++ main.cc
@@ -118,7 +118,7 @@ for(i = 0; i < number_of_consumers; i++) {
 cout << "..Created all consumer threads..";
 
 for(j = 0; j < number_of_producers; j++) {
-  cout << " j = " << j << endl;
+  cout << "\n in producer creation loop - j = " << j << endl;
       temp2[j] = j;
       pthread_create(&producer_threads[j], NULL, producer, (void*)&temp2[j]);
     //  cout << "checkpoint post pthread_create j = " << j << endl;
